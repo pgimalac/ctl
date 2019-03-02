8a44becd62a595fc0d41127a2d4e2c92526d9a3d
Alexandre Moine <alexandre@moine.me>
2019-03-02 10:00:55 +0100

    meilleure arborescence

--- lib/formule.ml
+++ lib/formule.ml
@@ -1,15 +1,23 @@
-type binop = And | Or | Implies
+type binop = And | Or
 
 type 'a state =
-  P of 'a
+  P of 'a (* Juste un état *)
 | B of bool
-| Not of 'a state
-| Binop of binop * 'a state * 'a state
+| SNot of 'a state
+| SBinop of binop * 'a state * 'a state
 
 type 'a formule =
-  EX of 'a state
+  D of 'a state (* Juste un état *)
+(* Combinateurs temporels *)
+| EX of 'a state
 | AX of 'a state
 | EU of 'a state * 'a state
 | AU of 'a state * 'a state
+(* Récursivité *)
+| FNot of 'a formule
+| FBinop of binop * 'a formule * 'a formule
 
+(* Le combinateur f *)
 let f x = EU (B true, x)
+(* Le combinateur g *)
+let g x = FNot (f (SNot x))
