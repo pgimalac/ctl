727012a2ab16ae2f3cfec5eacbd42fec47726b87
Alexandre Moine <alexandre@moine.me>
2019-03-04 19:46:41 +0100

    AU

--- lib/marqueur.ml
+++ lib/marqueur.ml
@@ -42,7 +42,7 @@ let rec marquage f m =
      let mpsi1 = marquageS psi1 m in
      let mpsi2 = marquageS psi2 m in
      let todo = ref [] in (* todo est L *)
-     let res =
+     let res = (* Map (res,dejavu) *)
        M.mapi
          (fun i _ ->
            if M.find i mpsi2
@@ -71,5 +71,35 @@ let rec marquage f m =
               m (xs,res) in
           tantque newlist res
      in tantque !todo res
-
-  | _ -> failwith "todo"
+  | AU (psi1,psi2) ->
+     let mpsi1 = marquageS psi1 m in
+     let mpsi2 = marquageS psi2 m in
+     let todo = ref [] in
+     let res = (* Map (rex, nb) *)
+       M.mapi
+         (fun q s ->
+           if M.find q mpsi2
+           then
+             todo := q :: !todo;
+           (false,S.cardinal s)
+         ) m in
+     let rec tantque todo res =
+       match todo with
+       | [] -> M.map fst res
+       | q::xs ->
+          let res = M.update q (opt_map (fun (_,y) -> (true,y))) res in
+          let (newlist,res) =
+            M.fold (fun q' s (newlist,res) ->
+                (* C'est un prédécesseur que l'on n'a pas vu *)
+                if S.mem q s
+                then
+                  let newqnb = ref (true,0) in
+                  let res = M.update q' (opt_map (fun (x,y) -> newqnb := (x,y-1); !newqnb)) res in
+                  if (snd !newqnb = 0) && (M.find q' mpsi1) && not (fst !newqnb)
+                  then (q'::newlist,res)
+                  else (newlist,res)
+                else (newlist,res)
+              )
+              m (xs,res) in
+          tantque newlist res
+     in tantque !todo res
