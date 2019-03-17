ff6a55adda2902129b77b2290b819038e9fb7b26
Alexandre Moine <alexandre@moine.me>
2019-03-17 14:09:28 +0100

    negation

--- lib/formule.ml
+++ lib/formule.ml
@@ -3,7 +3,7 @@ type tempUnop = AX | EX | AF | EF | AG | EG
 type tempBinop = EU | AU | EW | AW
 
 type 'a formule =
-(* Logique propositionnelle *)
+  (* Logique propositionnelle *)
   B of bool
 | P of 'a
 | Not of 'a formule
@@ -38,20 +38,52 @@ let getop c =
   | Eq -> (=)
 
 let print_formule printer f =
-    let rec to_string f =
-        match f with
-        | B(b) -> if b then "⊤" else "⊥"
-        | P(p) -> printer p
-        | Not (f) -> "¬ (" ^ (to_string f) ^")"
-        | Binop(b, phi, psi) ->
-           "(" ^ (to_string phi) ^ ") " ^ (get_string b) ^ " (" ^ (to_string psi) ^")"
-        | TempUnop(u, phi) -> (get_string_temp u) ^ " (" ^ (to_string phi) ^")"
-        | TempBinop(b, phi, psi) ->
-            let phi = to_string phi in
-            let psi = to_string psi in
-            match b with
-            | EU -> "E (" ^ phi ^ ") U (" ^ psi ^")"
-            | AU -> "A (" ^ phi ^ ") U (" ^ psi ^")"
-            | EW -> "E (" ^ phi ^ ") W (" ^ psi ^")"
-            | AW -> "A (" ^ phi ^ ") W (" ^ psi ^")"
-    in print_endline (to_string f)
+  let rec to_string f =
+    match f with
+    | B b -> if b then "⊤" else "⊥"
+    | P p -> printer p
+    | Not f -> "¬ (" ^ (to_string f) ^")"
+    | Binop(b, phi, psi) ->
+       "(" ^ (to_string phi) ^ ") " ^ (get_string b) ^ " (" ^ (to_string psi) ^")"
+    | TempUnop(u, phi) -> (get_string_temp u) ^ " (" ^ (to_string phi) ^")"
+    | TempBinop(b, phi, psi) ->
+       let phi = to_string phi in
+       let psi = to_string psi in
+       match b with
+       | EU -> "E (" ^ phi ^ ") U (" ^ psi ^")"
+       | AU -> "A (" ^ phi ^ ") U (" ^ psi ^")"
+       | EW -> "E (" ^ phi ^ ") W (" ^ psi ^")"
+       | AW -> "A (" ^ phi ^ ") W (" ^ psi ^")"
+  in print_endline (to_string f)
+
+let rec desc_neg f =
+  match f with
+  | B _ | P _ -> f
+  | Binop (b, phi, psi) -> Binop (b, desc_neg phi, desc_neg psi)
+  | TempUnop (u, phi) -> TempUnop (u, desc_neg phi)
+  | TempBinop (b, phi, psi) -> TempBinop (b, desc_neg  phi, desc_neg  psi)
+  | Not g ->
+     match g with
+     | P _ -> f
+     | B b -> B (not b)
+     | Binop (b, phi, psi) ->
+        let newop =
+          match b with
+          | And -> Binop (Or, Not phi, Not psi)
+          | Or -> Binop (And, Not phi, Not psi)
+          | Impl -> Binop (And, phi, Not psi)
+          | Xor -> Binop (Eq, phi, psi)
+          | Eq -> Binop (Xor, phi, psi)
+        in desc_neg newop
+     | TempUnop (u, phi) ->
+        let newop =
+          match u with
+            | AX -> TempUnop (EX, Not phi)
+            | EX -> TempUnop (AX, Not phi)
+            | AF -> TempUnop (EG, Not phi)
+            | EG -> TempUnop (AF, Not phi)
+            | EF -> TempUnop (AG, Not phi)
+            | AG -> TempUnop (EF, Not phi)
+        in desc_neg newop
+     | TempBinop _ -> failwith "todo"
+     | Not f -> desc_neg f
