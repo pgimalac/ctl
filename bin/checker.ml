382a2f0a66f7d1d3dd50bbee85d4993b66b0de2c
Alexandre Moine <alexandre@moine.me>
2019-03-31 15:05:48 +0200

    better examples....

--- bin/checker.ml
+++ bin/checker.ml
@@ -26,12 +26,14 @@ let check_formula_in kripke start x =
   print_string ("[" ^ string_of_formule (fun x -> x) (formule_from_pretty form) ^"]");
   print_endline ":";
   print_endline
-    (string_of_bool (Lib.Fpg.check (formule_from_pretty form) kripke start))
+    ("* Marquage : " ^ string_of_bool (T.check (formule_from_pretty form) kripke start));
+  print_endline
+    ("* Jeu      : " ^ string_of_bool (Lib.Fpg.check (formule_from_pretty form) kripke start))
 
 let main () =
-  (* print_endline "graph 1:";
+  print_endline "graph 1:";
   let fig2D1 = extract "graphs/g1.ctl" in
-  List.iter (check_formula_in fig2D1 0) (strings_of_file "graphs/f1.ctl"); *)
+  List.iter (check_formula_in fig2D1 0) (strings_of_file "graphs/f1.ctl");
   print_endline "\ngraph 2:";
   let g2 = extract "graphs/g2.ctl" in
   List.iter (check_formula_in g2 1) (strings_of_file "graphs/f2.ctl")
--- graphs/f2.ctl
+++ graphs/f2.ctl
@@ -2,7 +2,7 @@ EG a
 E a W z
 E a U c
 EX b
-A a W b
+E a W b
 EX (E a U c)
 - (EF z)
 AX (a | b)
